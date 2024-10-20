0feb638a1d8cb3e8ed2542b91bd3f747eafba200
SubhasishPalaur <subhasish@getvital.in>
2024-10-20 13:39:57 +0530

    logic for custom separator

--- src/Utils/StringCalculator.tsx
+++ src/Utils/StringCalculator.tsx
@@ -2,7 +2,19 @@ export function add(numbers: string): number {
     if (numbers === "") {
         return 0;
     }
-    const numArray = numbers.split(/[,\\n]+/);
+    let delimiter = /[,\\n]+/; 
+    let numbersToAdd = numbers;
+
+    if (numbers.startsWith("//")) {
+        const endOfDelimiterLine = numbers.indexOf("\\n");
+        const delimiterLine = numbers.substring(2, endOfDelimiterLine);
+        delimiter = new RegExp(`[,\\n${delimiterLine}]`); 
+        numbersToAdd = numbers.substring(endOfDelimiterLine + 2);
+        console.log(endOfDelimiterLine, delimiterLine, delimiter)
+    }
+
+    const numArray = numbersToAdd.split(delimiter).map((val)=>val.split('\\n')).flat();
+
     let sum = 0;
 
     for (const num of numArray) {
