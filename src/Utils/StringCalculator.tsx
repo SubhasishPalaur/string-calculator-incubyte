b2d9b2702a527059074826001c6b1f209ec965ce
SubhasishPalaur <subhasish@getvital.in>
2024-10-20 13:34:51 +0530

    logc for \n

--- src/Utils/StringCalculator.tsx
+++ src/Utils/StringCalculator.tsx
@@ -2,9 +2,7 @@ export function add(numbers: string): number {
     if (numbers === "") {
         return 0;
     }
-
-    const numArray = numbers.split(',');
-
+    const numArray = numbers.split(/[,\\n]+/);
     let sum = 0;
 
     for (const num of numArray) {
